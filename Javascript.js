49d010ffc44a054d63a8e82dd1b817a434970663
deep <deep@deeps-MacBook.local>
2016-01-09 09:24:14 -0800

    html template done. css still in woworks 60%. still working on the js.

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,69 @@
-function projects (title, description, date ) {
 
-	this.title =,
-	this.description =,
-	this.date =,
+$(function(){
+var projects = [];
+var $newProject =
 
+
+function Project (title, description, date) {
+
+this.title = title;
+this.description = description;
+this.date = tuesday;
 }
+
+/*Converting project object to HTML before displaying on site*/
+
+Project.prototype.toHtml = function () {
+var $newProject = $('.projectClass').clone();
+
+/*Populating the project li element that has class of .projectClass with most current project info: title, description, date */
+
+$newProject.find('.projectClass').text(this.title);
+$newProject.find('.projectClass').text(this.description);
+$newProject.find('.projectClass').text(this.date);
+};
+
+
+
+
+
+var projectOne = new Project('landing Page', 'Lake Tahoe Landing Page', '10/14/16');
+
+projectOne.toHtml('.projectClass');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// projectData.forEach(function(ele) {
+// projects.push(new Project(ele));
+//
+// project.forEach(function(a){
+// $('.projectClass').append(li.toHtml())
+
+
+
+
+    // projectData.forEach(function(ele) {
+    // projects.push(new Project(ele));
+    //
+    // project.forEach(function(a){
+    // $('.projectClass').append(li.toHtml())
+
+
+
+
+
+});
--- projectdata.js
+++ projectdata.js
@@ -1,8 +1,25 @@
 var projectData= [
   {
-    title:
-    description:
-    date:
+    title: "form",
+    description: "submission form",
+    date: "tuesday"
   },
 
-  ....
+  var projectData= [
+    {
+      title: "lake tahoe landing page",
+      description: "css basics",
+      date: "Friday"
+    },
+    var projectData= [
+      {
+        title:
+        description:
+        date:
+      },
+      var projectData= [
+        {
+          title:
+          description:
+          date:
+        },
