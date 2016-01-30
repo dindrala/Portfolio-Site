a89e8385dbeaab1730787d28196ce525dec46d6e
Randallonius <jonathanprandall@gmail.com>
2016-01-30 10:39:33 -0800

    wrapped projectView into IFFE

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,19 +1,21 @@
-var projectView= {};
+(function(module) {
+  var projectView= {};
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(e) { //on-click within the main-nav class that targets the tab class
-    $('.tab-content').hide(); //hides all selectors with tab-content class
-    $('#' + $(this).data('content')).fadeIn(); //fades in data-content of the # attri that was clicked
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function(e) { //on-click within the main-nav class that targets the tab class
+      $('.tab-content').hide(); //hides all selectors with tab-content class
+      $('#' + $(this).data('content')).fadeIn(); //fades in data-content of the # attri that was clicked
+    });
+
+    $('.main-nav .tab:first').click(); //triggers click on the first .tab element, to set up the page.
+  };
+
+
+
+  $(document).ready(function() {
+    ProjectView.handleMainMenu(); //runs the function when the browser load
   });
 
-  $('.main-nav .tab:first').click(); //triggers click on the first .tab element, to set up the page.
-};
-
-
-
-$(document).ready(function() {
-  ProjectView.handleMainMenu(); //runs the function when the browser load
-});
 
 
 
@@ -28,11 +30,12 @@ $(document).ready(function() {
 
 
 
+  projectView.initIndexPage = function() {
+    Sites.all.forEach(function(a){
+      $('#articles').append(a.toHtml());
+    });
+    projectView.handleMainMenu();
 
-projectView.initIndexPage = function() {
-  Sites.all.forEach(function(a){
-    $('#main').append(a.toHtml());
-  });
-  projectView.handleMainMenu();
-
-};
+  };
+  module.ProjectView = ProjectView;
+}) (window);
