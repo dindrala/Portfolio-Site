e67f6055d409ade9d5d738afe4d988b5a009da8c
Lisa Hackenberger <risachi@gmail.com>
2016-01-30 14:18:49 -0800

    fixed the tabs function

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -2,9 +2,17 @@
   var ProjectView= {};
 
   ProjectView.handleMainNav = function() {
-    $('.main-nav').on('click', '.tab', function(e) { //on-click within the main-nav class that targets the tab class
+    $('nav a').on('click', function(e) { //on-click within the main-nav class that targets the tab class
+      var tab = $(e.target);
+      var tabContent = tab.attr('data-content');
+      console.log(tabContent);
+      var contentTab = $('section[data-content=' + tabContent + ']');
+      console.log(contentTab);
+
       $('.tab-content').hide(); //hides all selectors with tab-content class
-      $('#' + $(this).data('content')).fadeIn(); //fades in data-content of the # attri that was clicked
+      // $('#' + $(this).data('content')).fadeIn(); //fades in data-content of the # attri that was clicked
+      contentTab.show();
+
     });
 
     $('.main-nav .tab:first').click(); //triggers click on the first .tab element, to set up the page.
