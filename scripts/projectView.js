5d4163bd19bf0294aef15c14a06812a8d6e436d1
deep <deep@deeps-MacBook.local>
2016-02-08 14:49:55 -0800

    everything is working.

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -3,21 +3,13 @@
   var ProjectView= {};
 
   ProjectView.handleMainNav = function() {
-    $('nav a').on('click', function(e) { //on-click within the main-nav class that targets the tab class
-      var tab = $(e.target);
-      var tabContent = tab.attr('data-content');
-      console.log(tabContent);
-      var contentTab = $('section[data-content=' + tabContent + ']');
-      console.log(contentTab);
-
-      $('.tab-content').hide(); //hides all selectors with tab-content class
-      // $('#' + $(this).data('content')).fadeIn(); //fades in data-content of the # attri that was clicked
-      contentTab.show();
-
-    });
-
-    $('.main-nav .tab:first').click(); //triggers click on the first .tab element, to set up the page.
+     $("nav a").on("click", function(e){
+     var dataContent = $(e.target).attr("data-content");
+     $("section").hide();
+     $("#" + dataContent).show();
+     });
   };
+
 
 
 
